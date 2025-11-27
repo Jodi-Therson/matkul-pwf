4b542c9d18a2dc9fa85d25dd68e91cf2dda97de0
Jodi-Therson <joditherson.26@gmail.com>
2025-11-27 10:48:40 +0700

    presentasi view and blade

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title')
+Beranda
+@endsection
+
+@section('content')
+<h1>Ini adalah Halaman Beranda</h1>
+<p>Selamat datang di bagian konten utama.</p>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\produkController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('halo', ['name' => 'Cok Ace']);
 });
 
 Route::get('/produk', [produkController::class, 'index']);
@@ -15,4 +15,8 @@ Route::get('/halaman', function () {
     $title = 'Harry Pooter';
     $content = 'harry potter and the deathly hallows: part 2';
     return view('halaman', compact('title', 'content'));
+});
+
+Route::get('/beranda', function () {
+    return view('beranda');
 });
